4ac4fdaf0c3dfda6590e6d6c08dc7af55d939fae
ewd1234 <jacoblorenzen@gmail.com>
2026-08-31 11:41:56 +0200

    min-første-api

---
+++ modul3-web-api-design/min-foerste-api/Program.cs
@@ -0,0 +1,48 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+List<string> frugterList = new List<string>
+{
+    "æble", "banan", "pære", "ananas"
+};
+
+String[] frugterArr = new String[]
+{
+    "æble", "banan", "pære", "ananas"
+};
+
+app.MapGet("/api/hello/", () => new { Message = "Hello world!" });
+
+app.MapGet("/api/hello/{name}", (string name) => new { Message = $"Hello {name}!"});
+
+app.MapGet("/api/hello/{name}/{age}", (string name, int age) => new { Message = $"Hej {name}, du er {age} år gammel!"});
+
+app.MapGet("/api/fruit", () => new {Message = frugterArr} );
+
+app.MapGet("/api/fruit/{index}", (int index) => new {Message = frugterArr[index]});
+
+app.MapGet("/api/fruit/random", () => new {Message = frugterArr[Random.Shared.Next(frugterArr.Count())]});
+
+app.MapPost("/api/fruit/list/", (Fruit fruit) =>
+{
+    frugterList.Add(fruit.name);
+
+    Console.WriteLine($"Tilføjet frugt: {fruit.name}");
+
+    return frugterList;
+});
+
+app.MapPost("/api/fruit/arr/", (Fruit fruit) =>
+{
+    frugterArr = frugterArr.Append(fruit.name).ToArray();
+
+    Console.WriteLine($"Tilføjet frugt: {fruit.name}");
+
+    return frugterArr;
+});
+
+
+
+
+app.Run();
+record Fruit(string name);
