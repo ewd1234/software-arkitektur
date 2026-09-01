3091b0730d6f2f77f11bf2c8b60db0e6b79a3e50
ewd1234 <jacoblorenzen@gmail.com>
2026-09-01 12:57:02 +0200

    LAvet samtlige tasks api'er

--- modul3-web-api-design/min-foerste-api/Program.cs
+++ modul3-web-api-design/min-foerste-api/Program.cs
@@ -11,12 +11,21 @@ String[] frugterArr = new String[]
     "æble", "banan", "pære", "ananas"
 };
 
+List<Todo> todos = new List<Todo>
+{
+    new Todo {Id = 1, IsDone = false, Title = "Opvask"},
+    new Todo {Id = 2, IsDone = false, Title = "Støvesuge"},
+    new Todo {Id = 3, IsDone = false, Title = "Lave mad"}
+};
+
+//Hello
 app.MapGet("/api/hello/", () => new { Message = "Hello world!" });
 
 app.MapGet("/api/hello/{name}", (string name) => new { Message = $"Hello {name}!"});
 
 app.MapGet("/api/hello/{name}/{age}", (string name, int age) => new { Message = $"Hej {name}, du er {age} år gammel!"});
 
+//Fruit
 app.MapGet("/api/fruit", () => new {Message = frugterArr} );
 
 app.MapGet("/api/fruit/{index}", (int index) => new {Message = frugterArr[index]});
@@ -52,8 +61,48 @@ app.MapPost("/api/fruit/arr/", (Fruit fruit) =>
     return Results.Ok(frugterArr);
 });
 
+//Todo Tasks
+app.MapGet("/api/tasks", () => new {Message = todos});
 
+app.MapGet("/api/tasks/{id}", (int id) =>
+{
+    Todo? chosenTask = todos.FirstOrDefault(t => t.Id == id);
 
+    return chosenTask;
+});
+
+app.MapPut("/api/tasks/{id}", (int id, Todo updatedTask) =>
+{
+    if (id < 0 || id >= todos.Count())
+    {
+        return Results.BadRequest($"Kan ikke finde id: {id}");
+    }
+
+    updatedTask = todos[id - 1];
+    updatedTask.IsDone = !updatedTask.IsDone;
+    
+    return Results.Ok(todos);
+
+});
+
+app.MapDelete("/api/tasks/{id}", (int id) =>
+{
+    if (id <= 0 || id >= todos.Count())
+    {
+        return Results.BadRequest($"Kan ikke finde id: {id}");
+    }
+
+    todos.Remove(todos[id - 1]);
+
+    return Results.Ok(todos);
+});
 
 app.Run();
 record Fruit(string name);
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Title { get; set; }
+    public bool IsDone { get; set; }
+}
